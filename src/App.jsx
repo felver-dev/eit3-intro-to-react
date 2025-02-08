9dca84c4317a05e44481d149b1504026bbaa74e3
felver-dev <felver.ngouala@gmail.com>
2025-02-08 18:08:08 +0100

    MapWithEvent

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
 import "./app.css";
 // import Form from "./components/Form";
 import Map from "./components/Map";
-
+import MapWithEvent from "./components/MapWithEvent";
 
 const App = () => {
   return (
     <div className="w-full h-screen">
-      <Map />
+      {/* <Map /> */}
       {/* <Form /> */}
+
+      <MapWithEvent />
     </div>
   );
 };
--- src/components/MapWithEvent.jsx
+++ src/components/MapWithEvent.jsx
@@ -1,60 +1,37 @@
-import { MapContainer } from "react-leaflet";
-import { useEffect, useState } from "react";
+import { useState } from "react";
+import { MapContainer, Marker, Popup, useMapEvents } from "react-leaflet";
+import CustomTileLayer from "./TileLayer";
 
-// const users = {
-//   id: int,
-//   name: string,
-//   firstName: string,
-//   isDriver: boolean,
-//   email: string,
-//   password: string,
-//   createdAt:dateTime(),
-//   updatedAt:dateTime()
-// };
-// const user1 = {
-//   id: 1,
-//   name: "Jules",
-//   firstName: "felver",
-//   isDriver: false,
-//   email: "jules@gmail.com",
-//   password: "jules12345",
-// };
-// const user2 = {
-//   id: 1,
-//   name: "Mayandza",
-//   firstName: "Ngouala",
-//   isDriver: true,
-//   email: "maya@gmail.com",
-//   password: "maya12345",
-// };
+const LocationMarker = () => {
+  const [position, setPosition] = useState(null);
 
-// if (user.isDrive){
-//   console.log(`${user.name} is a driver`);
-// }
-// else {
-//   console.log(`${user.name} is a client`);
-// }
+  const map = useMapEvents({
+    click() {
+      map.locate();
+    },
+    locationfound(event) {
+      setPosition(event.latlng);
+      map.flyTo(event.latlng, map.getZoom());
+    },
+  });
+
+  return position === null ? null : (
+    <Marker position={position}>
+      <Popup>Vous êtes ici même ! 😊</Popup>
+    </Marker>
+  );
+};
 
 const MapWithEvent = () => {
-  const [coordinates, setCoordinates] = useState({
-    lat: 5.345317,
-    lng: -4.024429,
-  });
-  const [driverCoordinates, setDriverCoordinates] = useState({
-    lat: 5.345317,
-    lng: -4.024429,
-  });
-
-  const handleCoordinatesModifier = (newLat, newLng) => {
-    setCoordinates({
-      lat: newLat,
-      lng: newLng,
-    });
-  };
   return (
     <MapContainer
-      center={{ lat: coordinates.lat, lng: coordinates.lng }}
-    ></MapContainer>
+      center={{ lat: 5.345317, lng: -4.024429 }}
+      zoom={18}
+      scrollWheelZoom={false}
+    >
+      <CustomTileLayer />
+      <LocationMarker />
+    </MapContainer>
   );
 };
 
